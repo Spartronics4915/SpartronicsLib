468369af0a82c8b40c00a522fa8ff2835cb4c8cc
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2019-05-05 15:21:45 -0700

    Add initial bits of T265 class

---
+++ src/main/java/com/spartronics4915/lib/sensors/T265Camera.java
@@ -0,0 +1,52 @@
+package com.spartronics4915.lib.sensors;
+
+import com.spartronics4915.lib.math.geometry.Pose2d;
+import com.spartronics4915.lib.math.geometry.Twist2d;
+
+import java.util.function.Supplier;
+
+/**
+ * Provides a convenient Java interface to the Intel RealSense
+ * T265 V-SLAM camera. Only the subset of the librealsense that is useful
+ * to robot tracking is exposed in this class.
+ * 
+ * We employ JNI to call librealsense. There _are_ Java bindings for
+ * librealsense, but they are not complete and do not support our usecase.
+ * 
+ * This class works entirely in 2d, even though the tracking camera supports
+ * giving us a third dimension.
+ * 
+ */
+public class T265Camera
+{
+    static {
+        System.loadLibrary("t265wrapper");
+    }
+
+    public T265Camera(Supplier<Pose2d> poseRecievedCallback,
+            String relocalizationMapPath, Pose2d cameraOffset, double odometryCovariance)
+    {
+        loadRelocalizationMap(relocalizationMapPath);
+        setOdometryInfo((float) cameraOffset.getTranslation().x(), (float) cameraOffset.getTranslation().y(),
+                (float) cameraOffset.getRotation().getDegrees(), odometryCovariance);
+        openAndStartCamera(poseRecievedCallback);
+    }
+
+    public native void startCamera();
+    public native void stopCamera();
+    public void sendOdometry(int sensorId, int frameNumber, Twist2d velocity)
+    {
+        Pose2d transVel = Pose2d.exp(velocity);
+        sendOdometryRaw(sensorId, frameNumber, (float) transVel.getTranslation().x(), (float) transVel.getTranslation().y());
+    }
+
+    private native void sendOdometryRaw(int sensorId, int frameNumber, float xVel, float yVel);
+    private native void openAndStartCamera(Supplier<Pose2d> poseRecievedCallback);
+    private native void loadRelocalizationMap(String path);
+    private native void setOdometryInfo(float offsetX, float offsetY, float offsetAng, double measurementCovariance);
+
+    // Thrown if something goes wrong in the native code
+    public class CameraJNIException extends RuntimeException
+    {
+    }
+}
---
+++ src/main/java/com/spartronics4915/lib/sensors/native/t265wrapper.cpp
@@ -0,0 +1,27 @@
+#include "t265wrapper.h"
+#include <librealsense2/rs.h> 
+
+void Java_com_spartronics4915_lib_sensors_T265Camera_stopCamera(JNIEnv *env, jobject thisClass)
+{
+    
+}
+
+void Java_com_spartronics4915_lib_sensors_T265Camera_sendOdometryRaw(JNIEnv *, jobject thisClass, jint sensorId, jint frameNumber, jfloat xVel, jfloat yVel)
+{
+
+}
+
+void Java_com_spartronics4915_lib_sensors_T265Camera_openAndStartCamera(JNIEnv *, jobject thisClass, jobject frameRecvCallback)
+{
+
+}
+
+void Java_com_spartronics4915_lib_sensors_T265Camera_loadRelocalizationMap(JNIEnv *, jobject thisClass, jstring mapPath)
+{
+
+}
+
+void Java_com_spartronics4915_lib_sensors_T265Camera_setOdometryInfo(JNIEnv *, jobject thisClass, jfloat xOffset, jfloat yOffset, jfloat angOffset, jfloat measureCovariance)
+{
+
+}
