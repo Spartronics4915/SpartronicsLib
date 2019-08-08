1c08032ec52f526562d9bb64855959d1eb0ff3fe
j-james <35242550+j-james@users.noreply.github.com>
2019-08-07 18:22:32 -0700

    Remove Override

--- src/main/java/com/spartronics4915/lib/subsystems/estimator/RobotStateEstimator.java
+++ src/main/java/com/spartronics4915/lib/subsystems/estimator/RobotStateEstimator.java
@@ -95,7 +95,6 @@ public class RobotStateEstimator extends SpartronicsSubsystem
         SmartDashboard.putNumber("RobotState/velocity", cstate.predictedVelocity.dx);
     }
 
-    @Override
     public void stop()
     {
         mSLAMCamera.stop();
