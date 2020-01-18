de4c0762d61a883a1927779e26b958b3271fd24e
Camden Greenhalgh <lego76666@gmail.com>
2020-01-18 14:53:13 -0800

    Fixed typos

--- src/main/java/com/spartronics4915/lib/hardware/motors/SensorModel.java
+++ src/main/java/com/spartronics4915/lib/hardware/motors/SensorModel.java
@@ -7,8 +7,8 @@ public class SensorModel {
      * This is a convenience constructor for sensors connected to a wheel. Use the
      * more direct constructor if your sensor is not connected to a wheel.
      * 
-     * @param wheelDiameterMeters      The diameter of your wheel in custom units.
-     * @param nativeUnitsPerRevolution The number of custom units per wheel revolution.
+     * @param wheelDiameterMeters      The diameter of your wheel in meters.
+     * @param nativeUnitsPerRevolution The number of native units per wheel revolution.
      */
     public static SensorModel fromWheelDiameter(double wheelDiameterMeters, double nativeUnitsPerRevolution) {
         return new SensorModel((1 / nativeUnitsPerRevolution) * (wheelDiameterMeters * Math.PI));
@@ -16,16 +16,17 @@ public class SensorModel {
 
     /**
      * @param nativeUnitsToCustomUnitsMultiplier A number that, when multiplied with some
-     *                                           amount of custom units, converts to meters.
-     *                                           This factor will also be used to convert
-     *                                           to related units, like custom units/sec.
+     *                                           amount of native units, converts to custom
+     *                                           units. This factor will also be used to 
+     *                                           convert to related units, like custom 
+     *                                           units/sec.
      */
     public static SensorModel fromMultiplier(double nativeUnitsToCustomUnitsMultiplier) {
         return new SensorModel(nativeUnitsToCustomUnitsMultiplier);
     }
 
     /**
-     * @param nativeUnitsPerRevolution The number of custom units per wheel revolution.
+     * @param nativeUnitsPerRevolution The number of native units per wheel revolution.
      */
     public static SensorModel toRadians(double nativeUnitsPerRevolution) {
         return new SensorModel((1 / nativeUnitsPerRevolution) * 2 * Math.PI);
